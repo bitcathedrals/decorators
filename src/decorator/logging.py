5ff959dacd693f3f5b2d1da4ced3854a69ef2ae3
Michael Mattie <codermattie@runbox.com>
2024-04-15 13:00:15 -0700

    (fix) conform to ecs standard

--- src/decorator/logging.py
+++ src/decorator/logging.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 from functools import wraps
 
 from json import dumps
@@ -5,39 +8,63 @@ from json import dumps
 import time
 from datetime import date, datetime
 
-def _get_formatted_time(time):
-    return time.strftime("%H:%M:%S")
+def _format_time(dt):
+    return dt.strftime("%H:%M:%S")
+
+def _format_date(now_object):
+    return dt.strftime("%Y-%B-%d")
 
 def _get_delta_seconds(start, end):
      delta = end - start
      return delta.total_seconds()
 
+SERVICE = os.getenv("SERVICE")
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+
 def crumb(func):
+    src_function = func.__name__
+
+    module = None
+    src_module = 'unknown'
+
+    if func.__module__:
+        module = func.__module__
+        src_module = module.__name__
+
+    src_file = 'unknown'
+
+    # the line is the second element in the tuple of func,line
+    src_line = inspect.getsourcelines(func)[1]
+
+    if module:
+        src_file = inspect.getfile(module)
 
     @wraps(func)
     def log_crumb(*args, correlation_id="None", **kwargs):
-        run_date = date.today()
+        start_dt = datetime.now()
+        iso9660 = start_dt.iso_format(),
 
-        start_time = datetime.now()
-
-        name = func.__name__
-
-        module = 'unknown'
-
-        if func.__module__:
-             module = func.__module__
-
-        mark = time.time()
+        call_id = src_function + "-" + str(time.time_ns())
 
         start_data = {
-            'date': run_date.strftime('%d-%b-%Y'),
-            'start_time':  _get_formatted_time(start_time),
-            'module': module,
-            'name': name,
-            'correlation_id': correlation_id, 
-            'arg_list': args,
-            'arg_keys': kwargs,
-            'timestamp': mark
+            '@timestamp': iso9660,
+            'log.level': "DEBUG",
+            'log.logger': "crumb",
+            'log.origin.file.name': src_file,
+            'log.origin.file.line': src_line,
+            'log.origin.function': src_function,
+
+            'message': "call"
+
+            'service.name': SERVICE,
+            'service.environment': ENVIRONMENT,
+
+            'gauge.correlation_id': correlation_id, 
+
+            'gauge.arg_list': repr(args),
+            'gauge.arg_keys': repr(kwargs),
+
+            'gauge.call.id' = call_id
         }
 
         print(dumps(start_data, indent=4))
@@ -47,16 +74,28 @@ def crumb(func):
 
         retval = func(*args, **kwargs)
 
-        end_time = datetime.now()
+        end_dt = datetime.now()
 
         end_data = {
-            'timestamp': mark,
-            correlation_id: correlation_id,
-            'end_time': _get_formatted_time(end_time), 
-            'module': module,
-            'name': name,
-            'runtime': "{:.3f}".format(_get_delta_seconds(start_time, end_time)),
-            'returned': retval
+            '@timestamp': iso9660,
+            'log.level': "DEBUG",
+            'log.logger': "crumb",
+            'log.origin.file.name': src_file,
+            'log.origin.file.line': src_line,
+            'log.origin.function': src_function,
+
+            'message': "return"
+
+            'service.name': SERVICE,
+            'service.environment': ENVIRONMENT,
+
+            'gauge.correlation_id': correlation_id, 
+
+            'gauge.return': repr(retval),
+
+            'gauge.call.id' = call_id,
+
+            'gauge.runtime': "{:.3f}".format(_get_delta_seconds(start_dt, end_dt)),
         }
 
         print(dumps(end_data, indent=4))
